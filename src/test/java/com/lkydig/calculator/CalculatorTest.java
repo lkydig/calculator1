75b12c825c08e65d91a9f4f5cfe174364408b6a0
lkydig <lkydig@gmail.com>
2018-05-15 15:43:03 +0900

    Add Dockerfile

--- src/test/java/com/lkydig/calculator/CalculatorTest.java
+++ src/test/java/com/lkydig/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.lkydig.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+/** this is public */
 public class CalculatorTest {
 	private Calculator calculator = new Calculator();
 
